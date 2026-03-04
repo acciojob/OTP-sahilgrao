17f4e9736744708511716333a695e10acab105e7
Sahil G Rao <92579906+sahilgrao@users.noreply.github.com>
2026-03-04 11:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+codes.forEach((code,index)=>{
+
+	code.addEventListener('keydown',(e)=>{
+
+		//If user types a number
+		if(e.key >= 0 && e.key <=9){
+			code.value = '';
+
+			setTimeout(()=>{
+				if (codes[index + 1]){
+					codes[index + 1].focus();
+				} 
+			},10);
+		}
+		//backspace behaviour
+		if(e.key === 'Backspace'){
+			setTimeout(()=>{
+				if (codes[index - 1]){
+					codes[index - 1].focus();
+				}
+			},10);
+		}
+	})
+})
