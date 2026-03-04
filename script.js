2a90680f966aa31fc6bca2cf59fab4eaff106977
Sahil G Rao <92579906+sahilgrao@users.noreply.github.com>
2026-03-04 13:18:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 //your JS code here. If required.
-const codes = document.querySelectorAll('.code');
+const codes = document.querySelectorAll(".code");
 
 codes[0].focus();
 
-codes.forEach((code,index)=>{
+codes.forEach((code, index) => {
 
-	code.addEventListener('keydown',(e)=>{
+  code.addEventListener("keydown", (e) => {
 
-		//If user types a number
-		if(e.key >= 0 && e.key <=9){
-			code.value = '';
+    if (e.key >= 0 && e.key <= 9) {
 
-			setTimeout(()=>{
-				if (codes[index + 1]){
-					codes[index + 1].focus();
-				} 
-			},4);
-		}
-		//backspace behaviour
-		if(e.key === 'Backspace'){
-			setTimeout(()=>{
-				if (codes[index - 1]){
-					codes[index - 1].focus();
-				}
-			},4);
-		}
-	})
-})
+      code.value = "";
+
+      setTimeout(() => {
+        if (codes[index + 1]) {
+          codes[index + 1].focus();
+        }
+      }, 10);
+    }
+
+    if (e.key === "Backspace") {
+
+      setTimeout(() => {
+        if (codes[index - 1]) {
+          codes[index - 1].focus();
+        }
+      }, 10);
+    }
+
+  });
+
+});
