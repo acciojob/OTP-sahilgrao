560df85c9328afdcb997b19f4b88e3b9f1e82fa8
Sahil G Rao <92579906+sahilgrao@users.noreply.github.com>
2026-03-08 09:52:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,34 +2,35 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 	const codes = document.querySelectorAll(".code");
-	
+
 	codes[0].focus();
-	
+
 	codes.forEach((code, index) => {
-	
-	  code.addEventListener("keydown", (e) => {
-	
-	    if (e.key >= 0 && e.key <= 9) {
-	
-	      code.value = "";
-	
-	      setTimeout(() => {
-	        if (codes[index + 1]) {
-	          codes[index + 1].focus();
-	        }
-	      }, 10);
-	    }
-	
-	    if (e.key === "Backspace") {
-	
-	      setTimeout(() => {
-	        if (codes[index - 1]) {
-	          codes[index - 1].focus();
-	        }
-	      }, 10);
-	    }
-	
-	  });
-});
+
+		code.addEventListener("keydown", (e) => {
+
+			if (/^[0-9]$/.test(e.key)) {
+
+				code.value = "";
+
+				setTimeout(() => {
+					if (codes[index + 1]) {
+						codes[index + 1].focus();
+					}
+				}, 10);
+			}
+
+			if (e.key === "Backspace") {
+
+				setTimeout(() => {
+					if (codes[index - 1]) {
+						codes[index - 1].focus();
+					}
+				}, 10);
+			}
+
+		});
+
+	});
 
 });
